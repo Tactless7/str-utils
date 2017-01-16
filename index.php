39441de8aa91af8d0b021484e11f18a11c1853aa
Juliane Blier <blier.juliane@gmail.com>
2017-01-16 11:11:21 +0100

    Ajout des arguments dans les fonction underline et italic

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 			return '<strong>'. $string .'</strong>';
 		}
 
-		public function italic(){
+		public function italic($string){
 			return '<em>'. $string .'</em>';
 		}
 
-		public function underline(){
+		public function underline($string){
 			return '<u>'. $string .'</u>';
 		}
 
